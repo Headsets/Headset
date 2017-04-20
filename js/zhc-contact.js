a26e40b81913338bf992ff6452ea3c4fe0de8392
Mrzhanghc <>
2017-04-21 02:23:45 +0800

    '链接'

--- js/zhc-contact.js
+++ js/zhc-contact.js
@@ -17,4 +17,39 @@ $(document).ready(function(){
 		}
 	})
 	
+	//	表单验证
+	$('#form').validate({
+		rules:{
+			name:{
+				required:true,
+				maxlength:8 
+			},
+			email:{
+				required:true,
+				maxlength:8 
+			},
+			mob:{
+				required:true,
+			}
+		},
+		messages:{
+			name:{
+				required:'用户名不能为空',
+				maxlength:$.validator.format("请输入一个 长度最多是 {0} 的字符串")
+			},
+			email:{
+				required:'验证码不能为空',
+				email:true,
+			},
+			mob:{
+				required:'MOB不能为空'
+			}
+		},
+		errorElement:'div',
+		errorPlacement:function(erry,val){
+			erry.css({color:'red',fontSize:'12px',marginTop:'2px',width:'98%',height:'16px',float:'left',paddingLeft:'2%'}).insertAfter(val)
+		},
+
+	})
+	
 })
